29de26e65519808c1335e14678a1c661b6383c95
Kawooya Raymond <kawooyaraymond380@gmail.com>
2025-08-14 10:34:26 +0000

    webpack-240a9a8821801e13.js:1 Uncaught ChunkLoadError: Loading chunk 177

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  // Use relative paths for assets when building on GitHub Actions for a custom domain
+  assetPrefix: isGithubActions ? '' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
